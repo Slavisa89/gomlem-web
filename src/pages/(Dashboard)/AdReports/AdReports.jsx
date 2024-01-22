d80eda3784cfc0fa1bc52784e1680bcebc84b9fa
Lazar996F <furtulal96@gmail.com>
2024-01-22 10:52:07 +0100

    ad-reports: first commit

--- src/pages/(Dashboard)/AdReports/AdReports.jsx
+++ src/pages/(Dashboard)/AdReports/AdReports.jsx
@@ -1,7 +1,49 @@
 import React from 'react'
+import StatusBox from "@components/StatusBox";
+import Statistics from "@components/Statistics";
+import Table from "@components/NewTable";
+import Thead from "@components/NewTable/Thead";
+import Tbody from "@components/NewTable/Tbody";
 
 export default function AdReports() {
+  const dataHead = ["Customer name", "Date Paid", "Company Name", "Locations Targeted", "Amount Paid", "Ad Reports"];
+
   return (
-    <div>AdReports</div>
+    <div className="w-[70%]">
+    <div className='flex gap-[26px]'>
+      <StatusBox
+            typeBox="boxTypeOne"
+            color="red"
+            icon="dollar"
+            title="Total Revenue"
+            number="$50,000"
+            profit="12.76"
+            lastSince="Since last month"
+      />
+      <StatusBox
+            typeBox="boxTypeOne"
+            color="green"
+            icon="person"
+            title="Total Paying Customers"
+            number={7}
+            profit="12.76"
+            lastSince="Since last month"
+      />
+          <StatusBox
+            typeBox="boxTypeTwo"
+            color="blue"
+            icon="setting"
+            title="Ad Report Settings"
+            fnViewTitle="View And Edit"
+          />
+    </div>
+    <Statistics/>
+    <Table typeTable="helpRequests">
+        <Thead dataHead={dataHead} />
+        <Tbody isPending={false}>
+          
+        </Tbody>
+      </Table>
+    </div>
   )
 }
