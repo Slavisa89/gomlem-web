942558152ed9f474bcce0f2602b199fd85bec1f0
Slavisa Markovic <sslavishaa@gmail.com>
2024-01-26 22:38:12 +0100

    two modals top align

--- src/pages/(Dashboard)/AdReports/AdReports.jsx
+++ src/pages/(Dashboard)/AdReports/AdReports.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Statistics from "@components/Statistics";
 import TopLocations from "./components/TopLocations";
 import { StatusBoxContainer } from "./components/StatusBoxContainer/StatusBoxContainer";
